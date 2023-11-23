18aed3ad732c8a6dcaceacb22a7b7feb600277a4
JeanArce <113121403+JeanArce@users.noreply.github.com>
2023-11-23 16:04:09 +0100

    profile data and create listing modal

--- src/scripts/apis.mjs
+++ src/scripts/apis.mjs
@@ -1,40 +1,65 @@
+//accessToken - string
+//avatar - string
+//credits - number
+//email - string
+//profileName - string
+const storedUser = JSON.parse(localStorage.getItem('user'));
 const baseUrl = 'https://api.noroff.dev/api/v1';
 
 /**
- * 
- * @param {*} data 
- * @returns 
+ *
+ * @param {*} data
+ * @returns
  */
-export const registerApi = async(data) => {
-    const registerEndpoint = baseUrl + '/auction/auth/register';
-    const executeRegister = await fetch(registerEndpoint, {
-      method: 'POST',
-      body: JSON.stringify(data),
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-      },
-    });
+export const registerApi = async (data) => {
+  const registerEndpoint = baseUrl + '/auction/auth/register';
+  const executeRegister = await fetch(registerEndpoint, {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
 
-    const registerData = await executeRegister.json();
-    return registerData;
+  const registerData = await executeRegister.json();
+  return registerData;
 };
 
 /**
- * 
- * @param {*} data 
- * @returns 
+ *
+ * @param {*} data
+ * @returns
  */
+export const loginApi = async (data) => {
+  const loginEndpoint = baseUrl + '/auction/auth/login';
+  const executeLogin = await fetch(loginEndpoint, {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'content-type': 'application/json; charset=UTF-8',
+    },
+  });
 
-export const loginApi = async(data) => {
-    const loginEndpoint = baseUrl + '/auction/auth/login';
-    const executeLogin = await fetch(loginEndpoint, {
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers: {
-            'content-type': 'application/json; charset=UTF-8',
-        },
-    });
+  const loginData = await executeLogin.json();
+  return loginData;
+};
 
-    const loginData = await executeLogin.json();
-    return loginData;
- };
+
+/**
+ * 
+ * @param {*} profileName 
+ * @returns 
+ * gets profile data, pass profileName
+ */
+export const getProfileDetails = async (profileName) => {
+  const profileEndpoint = baseUrl + `/auction/profiles/${profileName}`;
+  const getProfileData = await fetch(profileEndpoint, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${storedUser.accessToken}`,
+    },
+  });
+
+  const profileData = await getProfileData.json();
+  return profileData;
+};
--- src/scripts/profile.js
+++ src/scripts/profile.js
@@ -1,3 +1,73 @@
+import { getProfileDetails } from './apis.mjs';
 import { redirectToLoginIfNotAuthenticated } from './helpers.mjs';
 
 redirectToLoginIfNotAuthenticated();
+
+const queryString = document.location.search;
+const urlParams = new URLSearchParams(queryString);
+const profileName = urlParams.get('profileName');
+
+const getProfileData = async () => {
+  const profileData = await getProfileDetails(profileName);
+  //console.log(profileData);
+
+  const profile = document.querySelector('.profile-value');
+  const email = document.querySelector('.email-value');
+  const credits = document.querySelector('.credit-score');
+
+  profile.innerHTML = profileData.name;
+  email.innerHTML = profileData.email;
+  credits.innerHTML = profileData.credits;
+};
+
+getProfileData();
+
+const addTagBtn = document.getElementById('addTagBtn');
+addTagBtn.addEventListener('click', (evt) => {
+  evt.preventDefault();
+
+  const input = document.createElement('input');
+  input.type = 'text';
+  input.name = 'tag';
+  input.classList.add('form-control');
+  input.classList.add('mb-3');
+
+  const tagsContainer = document.getElementById('tagsContainer');
+  tagsContainer.appendChild(input);
+});
+
+const addMediaBtn = document.getElementById('addMediaBtn');
+addMediaBtn.addEventListener('click', (evt) => {
+  evt.preventDefault();
+
+  const input = document.createElement('input');
+  input.type = 'text';
+  input.name = 'media';
+  input.classList.add('form-control');
+  input.classList.add('mb-3');
+
+  const mediaContainer = document.getElementById('mediaContainer');
+  mediaContainer.appendChild(input);
+});
+
+const createListingForm = document.getElementById('createListingForm');
+createListingForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  const formData = new FormData(evt.target);
+
+  const title = formData.get('title');
+  const description = formData.get('description');
+  const tag = formData.getAll('tag');
+  const media = formData.getAll('media');
+  const endsAt = formData.get('endsAt');
+
+  const data = {
+    title: title,
+    description: description,
+    tags: tag,
+    media: media,
+    endsAt: new Date(endsAt),
+  };
+
+  console.log(data);
+});
